55bf3fad131979a1062711bc4c04586afcea2054
Tal Zichlinsky <talzhtc@gmail.com>
2020-12-08 16:36:35 +0200

    Implemented init

--- src/api/DWGraph_Algo.java
+++ src/api/DWGraph_Algo.java
@@ -1,2 +1,53 @@
-package api;public class DWGraph_Algo {
+package api;
+
+import java.util.List;
+
+public class DWGraph_Algo implements dw_graph_algorithms{
+
+    private directed_weighted_graph graph;
+
+    /**
+     * shallow copies specified graph g to this graph.
+     * @param g
+     */
+    @Override
+    public void init(directed_weighted_graph g) {
+        if (g != null) this.graph = g;
+    }
+
+
+    @Override
+    public directed_weighted_graph getGraph() {
+        return null;
+    }
+
+    @Override
+    public directed_weighted_graph copy() {
+        return null;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+
+    @Override
+    public double shortestPathDist(int src, int dest) {
+        return 0;
+    }
+
+    @Override
+    public List<node_data> shortestPath(int src, int dest) {
+        return null;
+    }
+
+    @Override
+    public boolean save(String file) {
+        return false;
+    }
+
+    @Override
+    public boolean load(String file) {
+        return false;
+    }
 }
