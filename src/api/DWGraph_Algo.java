3a5ecd0a7649e9fc57bc1a706957e1170a452992
Tal Zichlinsky <talzhtc@gmail.com>
2020-12-14 14:40:16 +0200

    Fixed Kosaraju
    Now using Iterative DFS

--- src/api/DWGraph_Algo.java
+++ src/api/DWGraph_Algo.java
@@ -6,7 +6,7 @@ import org.json.simple.parser.*;
 import java.io.*;
 import java.util.*;
 
-public class DWGraph_Algo implements dw_graph_algorithms{
+public class DWGraph_Algo implements dw_graph_algorithms {
 
     private directed_weighted_graph graph;
     private HashMap<node_data, node_data> parents = new HashMap<>();
@@ -14,6 +14,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
     /**
      * shallow copies specified graph g to this graph.
+     *
      * @param g
      */
     @Override
@@ -28,52 +29,48 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
     /**
      * This method produces and returns a deep copy of the underlying graph of this class
+     *
      * @return directed_weighted_graph - a deep copy of the underlying graph of this class
      */
     @Override
     public directed_weighted_graph copy() {
-        if(graph == null) return null;
+        if (graph == null) return null;
 
         //The graph we will be returning
         directed_weighted_graph copy = new DWGraph_DS();
 
         //If original graph is empty, a new empty graph will bu sufficient to return.
-        if(graph.edgeSize() == 0 && graph.nodeSize() == 0) return copy;
+        if (graph.edgeSize() == 0 && graph.nodeSize() == 0) return copy;
 
         //We know those are not null because we passed the former if statement
         Collection<node_data> thisNodes = graph.getV();
         Collection<edge_data> currNeighbors;
 
         //run on all the nodes of this graph
-        for(node_data currNode: thisNodes)
-        {
+        for (node_data currNode : thisNodes) {
             int currKey = currNode.getKey();
 
             //If copy does not contain a node with currKey
-            if(copy.getNode(currKey) == null)
-            {
+            if (copy.getNode(currKey) == null) {
                 //copy the current node and add him to the copy graph
                 node_data copiedNode = new NodeData(currNode);
                 copy.addNode(copiedNode);
             }
             //run on all the edges going out from the current node if exist
             currNeighbors = graph.getE(currKey);
-            if(currNeighbors != null) {
+            if (currNeighbors != null) {
 
-                for(edge_data currEdge : currNeighbors)
-                {
+                for (edge_data currEdge : currNeighbors) {
                     int destKey = currEdge.getDest();
                     double currEdgeWeight = currEdge.getWeight();
-                    if(copy.getEdge(currKey,destKey) == null)
-                    {
-                        if (copy.getNode(destKey) == null)
-                        {
+                    if (copy.getEdge(currKey, destKey) == null) {
+                        if (copy.getNode(destKey) == null) {
                             node_data origDest = graph.getNode(destKey);
                             node_data copiedDest = new NodeData(origDest);
                             copy.addNode(copiedDest);
 
                         }
-                        copy.connect(currKey,destKey,currEdgeWeight);
+                        copy.connect(currKey, destKey, currEdgeWeight);
 
                     }
                 }
@@ -88,6 +85,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
      * A method to check whether a graph is strongly connected.
      * This method uses  Kosaraju's Algorithm and a DFS algorithm to count the number of SCC in the graph,
      * if it's more than one, the graph is not strongly connected.
+     *
      * @return True iff  this graph is strongly connected, meaning, every node in this graph can be reached
      * from every other node in this graph.
      */
@@ -119,7 +117,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
         pathLength = destination.getWeight();
 
-        if(pathLength == Double.POSITIVE_INFINITY) pathLength = -1;
+        if (pathLength == Double.POSITIVE_INFINITY) pathLength = -1;
 
         return pathLength;
 
@@ -135,7 +133,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
         ArrayList<node_data> lst = new ArrayList<>(); // list represent the path
         lst.add(son);
         if (size == 0) return lst; // if the list is empty return
-        for (node_data pointer : parents.values()){
+        for (node_data pointer : parents.values()) {
             dad = parents.get(son);
             lst.add(dad);
             if (dad == source) break;
@@ -152,7 +150,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
     public boolean save(String file) {
 
         if (!file.endsWith(".json") && file.contains(".")) return false;
-        else if(!file.endsWith(".json") && !file.contains("."))
+        else if (!file.endsWith(".json") && !file.contains("."))
             file = file + ".json";
 
         //We will write this JSON object to our file
@@ -163,8 +161,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
         JSONObject node;
         JSONObject edge;
 
-        for(node_data currNode : graph.getV())
-        {
+        for (node_data currNode : graph.getV()) {
             node = new JSONObject();
             node.put("key", currNode.getKey());
             node.put("info", currNode.getInfo());
@@ -172,7 +169,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
             node.put("tag", currNode.getTag());
             nodes.add(node);
             Collection<edge_data> myNeis = graph.getE(currNode.getKey());
-            if(myNeis != null && !myNeis.isEmpty()) {
+            if (myNeis != null && !myNeis.isEmpty()) {
                 for (edge_data currEdge : graph.getE(currNode.getKey())) {
                     edge = new JSONObject();
                     edge.put("src", currEdge.getSrc());
@@ -189,14 +186,11 @@ public class DWGraph_Algo implements dw_graph_algorithms{
         thisGraph.put("edge size", graph.edgeSize());
 
 
-        try(FileWriter fw = new FileWriter(file))
-        {
+        try (FileWriter fw = new FileWriter(file)) {
             fw.write(thisGraph.toString());
             fw.flush();
             return true;
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
@@ -208,11 +202,10 @@ public class DWGraph_Algo implements dw_graph_algorithms{
         JSONParser parser = new JSONParser();
 
         if (!file.endsWith(".json") && file.contains(".")) return false;
-        else if(!file.endsWith(".json") && !file.contains("."))
+        else if (!file.endsWith(".json") && !file.contains("."))
             file = file + ".json";
 
-        try
-        {
+        try {
 
             JSONObject myGraph = (JSONObject) parser.parse(new FileReader(file));
 
@@ -221,48 +214,42 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
             node_data node;
 
-            if(graph == null) graph = new DWGraph_DS();
+            if (graph == null) graph = new DWGraph_DS();
             clearGraph();
 
 
             Iterator<JSONObject> nodeIterator = Jnodes.iterator();
-            while(nodeIterator.hasNext())
-            {
+            while (nodeIterator.hasNext()) {
                 JSONObject jNode = nodeIterator.next();
-                long key = (long)jNode.get("key");
+                long key = (long) jNode.get("key");
                 double weight = (double) jNode.get("weight");
                 long tag = (long) jNode.get("tag");
                 String info = (String) jNode.get("info");
 
-                node = new NodeData((int)key, weight, info, (int)tag);
+                node = new NodeData((int) key, weight, info, (int) tag);
                 graph.addNode(node);
             }
 
             Iterator<JSONObject> edgeIterator = Jedges.iterator();
-            while(edgeIterator.hasNext())
-            {
+            while (edgeIterator.hasNext()) {
                 JSONObject jEdge = edgeIterator.next();
                 long src = (long) jEdge.get("src");
                 long dest = (long) jEdge.get("dest");
-                double weight  = (double) jEdge.get("weight");
+                double weight = (double) jEdge.get("weight");
 
-                graph.connect((int)src,(int)dest,weight);
+                graph.connect((int) src, (int) dest, weight);
             }
             return true;
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             return false;
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
-        }
-        catch (ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
             return false;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
@@ -273,9 +260,10 @@ public class DWGraph_Algo implements dw_graph_algorithms{
     /**
      * This method is a java implementation of Dijkstra's algorithm to find shortest path between two vertices in
      * a graph.
+     *
      * @param src
      */
-    private void Dijkstra(node_data src){
+    private void Dijkstra(node_data src) {
 
         //This queue will track which node we want to explore next
         PriorityQueue<node_data> pq = new PriorityQueue<>();
@@ -293,10 +281,10 @@ public class DWGraph_Algo implements dw_graph_algorithms{
             src.setInfo(visited);
 
             //If this node has no outgoing edges, there is nothing more to explore
-            if(graph.getE(src.getKey()) == null) continue;
+            if (graph.getE(src.getKey()) == null) continue;
 
             //Exploring this node's outgoing edges
-            for (edge_data pointer : graph.getE(src.getKey())){
+            for (edge_data pointer : graph.getE(src.getKey())) {
                 node_data destNode = graph.getNode(pointer.getDest());
 
                 //If the current path is shorter than the older one, replace nodes weight the correct one
@@ -305,14 +293,14 @@ public class DWGraph_Algo implements dw_graph_algorithms{
                     destNode.setWeight(pathWeight);
 
                     //Mark current src as dest's current 'cheapest' perant
-                    parents.put(destNode,src);
+                    parents.put(destNode, src);
                 }
 
                 if (destNode.getInfo().equals(unvisited)) {
                     pq.add(destNode);
                 }
             }
-         }
+        }
     }
 
     /**
@@ -320,13 +308,13 @@ public class DWGraph_Algo implements dw_graph_algorithms{
      * a SCC is a component in a graph in which from every vertex there is a path to every other vertex.
      * If the number of SCCs in the graph is 1, that means the graph itself is a SCC thus, th graph is connected.
      */
-    private int Kosaraju(){
+    private int Kosaraju() {
         //Pointers
         Collection<node_data> nodes = this.graph.getV();
         int nodeSize = graph.nodeSize();
 
         //If the graph is empty or has only one node it is vacuously connected
-        if(nodeSize <= 1) return 1;
+        if (nodeSize <= 1) return 1;
 
         //This stack will keep track of which node finished exploring its children
         Stack<Integer> KStack = new Stack<>();
@@ -339,10 +327,8 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
         setUnvisited(graph);
 
-        for (node_data node : nodes)
-        {
-            if(node.getInfo() == unvisited)
-            {
+        for (node_data node : nodes) {
+            if (node.getInfo() == unvisited) {
                 node.setInfo(visited);
                 DFSFill(KStack, DFSStack, node);
             }
@@ -351,10 +337,9 @@ public class DWGraph_Algo implements dw_graph_algorithms{
         directed_weighted_graph reversed = reverseGraph(graph);
         setUnvisited(reversed);
 
-        while (!KStack.isEmpty())
-        {
+        while (!KStack.isEmpty()) {
             Set<Integer> comp = DFSEmpty(DFSStack, SCC, reversed, KStack.pop());
-            if(comp != null) SCC.add(comp);
+            if (comp != null) SCC.add(comp);
         }
 
 
@@ -363,16 +348,16 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
     /**
      * This method is an implementation of a DFS algorithm. We use it in order to empty the stack Kosaraju's algorithm is using.
+     *
      * @param DFSStack - The stack that will help us maintain a DFS
-     * @param SCC - The set that will contain all of the nodes in the current SCC
-     * @param graphT - The reversed graph we will be working on
+     * @param SCC      - The set that will contain all of the nodes in the current SCC
+     * @param graphT   - The reversed graph we will be working on
      */
     private Set<Integer> DFSEmpty(Stack<Integer> DFSStack, Set<Set<Integer>> SCC, directed_weighted_graph graphT, int currKey) {
         //Pointer
         node_data currNode = graphT.getNode(currKey);
 
-        if (currNode.getInfo() == unvisited)
-        {
+        if (currNode.getInfo() == unvisited) {
             currNode.setInfo(visited);
 
             //We will be filling and returning this set
@@ -381,27 +366,25 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
             DFSStack.push(currNode.getKey());
 
+            OUTER:
             while (!DFSStack.isEmpty()) {
 
-                if (graphT.getE(DFSStack.peek()) == null)
-                {
+                if (graphT.getE(DFSStack.peek()) == null) {
                     DFSStack.pop();
                     continue;
                 }
 
-                Collection<edge_data> edges = graphT.getE(DFSStack.pop());
-                INNER:
-                for (edge_data edge : edges)
-                {
+                Collection<edge_data> edges = graphT.getE(DFSStack.peek());
+                for (edge_data edge : edges) {
                     node_data currChild = graphT.getNode(edge.getDest());
-                    if(currChild.getInfo() == unvisited)
-                    {
+                    if (currChild.getInfo() == unvisited) {
                         currChild.setInfo(visited);
                         DFSStack.push(currChild.getKey());
                         comp.add(currChild.getKey());
-                        break INNER;
+                        continue OUTER;
                     }
                 }
+                DFSStack.pop();
             }
             return comp;
         }
@@ -410,38 +393,37 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
     /**
      * This method is an implementation of a DFS algorithm. We use it in order to fill the stack Kosaraju's algorithm is using.
+     *
      * @param node
      * @param KStack
      * @param
      */
-    private void DFSFill(Stack<Integer> KStack, Stack<Integer> DFSStack, node_data node){
+    private void DFSFill(Stack<Integer> KStack, Stack<Integer> DFSStack, node_data node) {
         DFSStack.push(node.getKey());
 
-        while(!DFSStack.isEmpty())
-        {
-            if(graph.getE(DFSStack.peek()) == null)
-            {
+
+        OUTER:
+        while (!DFSStack.isEmpty()) {
+            if (graph.getE(DFSStack.peek()) == null) {
                 KStack.push(DFSStack.pop());
                 continue;
             }
             Collection<edge_data> edges = graph.getE(DFSStack.peek());
-            INNER:
-            while(true) {
-                for (edge_data edge : edges) {
-                    node_data currChild = graph.getNode(edge.getDest());
-                    if (currChild.getInfo() == unvisited) {
-                        currChild.setInfo(visited);
-                        DFSStack.push(currChild.getKey());
-                        break INNER;
-                    }
+
+
+            for (edge_data edge : edges) {
+                node_data currChild = graph.getNode(edge.getDest());
+                if (currChild.getInfo() == unvisited) {
+                    currChild.setInfo(visited);
+                    DFSStack.push(currChild.getKey());
+                    continue OUTER;
                 }
-                KStack.push(DFSStack.pop());
-                break;
             }
+            KStack.push(DFSStack.pop());
         }
     }
 
-    private directed_weighted_graph reverseGraph(directed_weighted_graph graph){
+    private directed_weighted_graph reverseGraph(directed_weighted_graph graph) {
         //Pointers
         Collection<edge_data> edges;
 
@@ -450,12 +432,11 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
         setEdgesInfo(reversed);
 
-        for(node_data node : graph.getV())
-        {
+        for (node_data node : graph.getV()) {
             edges = graph.getE(node.getKey());
-            if(edges != null && !edges.isEmpty()){
-                for(edge_data edge : edges){
-                    if(graph.getEdge(edge.getDest(),edge.getSrc()) == null) {
+            if (edges != null && !edges.isEmpty()) {
+                for (edge_data edge : edges) {
+                    if (graph.getEdge(edge.getDest(), edge.getSrc()) == null) {
                         if (reversed.getEdge(edge.getSrc(), edge.getDest()).getInfo() != flipped) {
                             reversed.removeEdge(edge.getSrc(), edge.getDest());
                             reversed.connect(edge.getDest(), edge.getSrc(), edge.getWeight());
@@ -470,16 +451,15 @@ public class DWGraph_Algo implements dw_graph_algorithms{
 
     /**
      * Sets info of all edges in given graph to "not flipped". It is used to reverse a graph
+     *
      * @param reversed
      */
     private void setEdgesInfo(directed_weighted_graph reversed) {
         String notFlipped = "not flipped";
-        for(node_data node : reversed.getV())
-        {
+        for (node_data node : reversed.getV()) {
             Collection<edge_data> edges = reversed.getE(node.getKey());
-            if(edges != null) {
-                for (edge_data edge : edges)
-                {
+            if (edges != null) {
+                for (edge_data edge : edges) {
                     edge.setInfo(notFlipped);
                 }
             }
@@ -489,7 +469,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
     /**
      * Sets all tags of nodes to positive infinity and all infos to "white"
      */
-    private void infWeights(){
+    private void infWeights() {
         node_data node;
         Iterator<node_data> iter = graph.getV().iterator(); // run on the HashMap gr
         while (iter.hasNext()) {
@@ -502,7 +482,7 @@ public class DWGraph_Algo implements dw_graph_algorithms{
     /**
      * Sets the info of all nodes in caller graph to "unvisitd"
      */
-    private void setUnvisited(directed_weighted_graph g){
+    private void setUnvisited(directed_weighted_graph g) {
         node_data node;
         Iterator<node_data> iter = g.getV().iterator();
         while (iter.hasNext()) {
@@ -511,10 +491,9 @@ public class DWGraph_Algo implements dw_graph_algorithms{
         }
     }
 
-    private void clearGraph(){
+    private void clearGraph() {
         if (graph == null) return;
-        for(node_data currNode : graph.getV())
-        {
+        for (node_data currNode : graph.getV()) {
             graph.removeNode(currNode.getKey());
         }
     }
