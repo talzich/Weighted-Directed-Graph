69e9f28bc31e38cc49367fe0f4e81f24645aed68
Tal Zichlinsky <talzhtc@gmail.com>
2020-12-18 09:45:19 +0200

    Reformated Pokemon

--- src/gameClient/Pokemon.java
+++ src/gameClient/Pokemon.java
@@ -10,15 +10,43 @@ public class Pokemon {
 	private Point3D pos;
 	private double minDist;
 	private int minRo;
-	
+
+	/**
+	 * A simple constructor
+	 * @param p
+	 * @param t
+	 * @param v
+	 * @param e
+	 */
 	public Pokemon(Point3D p, int t, double v, edge_data e) {
-		type = t;
-		value = v;
-		setEdge(e);
-		pos = p;
-		minDist = -1;
-		minRo = -1;
+		this.type = t;
+		this.value = v;
+		this.edge = e;
+		this.pos = p;
+		this.minDist = -1;
+		this.minRo = -1;
 	}
+
+
+	/**
+	 * A simple toString method
+	 * @return
+	 */
+	public String toString() {return "Pokemon:{value="+ value +", type= "+ type +"}";}
+
+
+	// ********** Getters & Setters ********** //
+	public edge_data getEdge() { return edge; }
+
+	public void setEdge(edge_data edge) { this.edge = edge; }
+
+	public Point3D getLocation() { return pos; }
+
+	public int getType() { return type; }
+	// ********** Getters & Setters ********** //
+
+
+	// ********** Currently unused ********** //
 	public static Pokemon init_from_json(String json) {
 		Pokemon ans = null;
 		try {
@@ -31,35 +59,15 @@ public class Pokemon {
 		}
 		return ans;
 	}
-	public String toString() {return "F:{v="+ value +", t="+ type +"}";}
-	public edge_data getEdge() {
-		return edge;
-	}
 
-	public void setEdge(edge_data edge) {
-		this.edge = edge;
-	}
+	public double getValue() { return value; }
 
-	public Point3D getLocation() {
-		return pos;
-	}
-	public int getType() {return type;}
-//	public double getSpeed() {return _speed;}
-	public double getValue() {return value;}
+	public double getMinDist() { return minDist; }
 
-	public double getMinDist() {
-		return minDist;
-	}
+	public void setMinDist(double mid_dist) { this.minDist = mid_dist; }
 
-	public void setMinDist(double mid_dist) {
-		this.minDist = mid_dist;
-	}
+	public int getMinRo() { return minRo; }
 
-	public int getMinRo() {
-		return minRo;
-	}
-
-	public void setMinRo(int minRo) {
-		this.minRo = minRo;
-	}
+	public void setMinRo(int minRo) { this.minRo = minRo; }
+	// ********** Currently unused ********** //
 }
